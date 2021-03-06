4f90ebc3125c8f59530d91e42d590e6a7ab8be8d
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-03-06 23:46:23 +0100

    Import data's API about the description's weather on the first TD of TBody

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+const lang = navigator.language.substr(0,2).toLowerCase();
+
 const options = {
     enableHighAccuracy: true,
     timeout: 5000,
@@ -10,10 +12,8 @@ navigator.geolocation.getCurrentPosition( pos => {
     
         let latitude = pos.coords.latitude;
         let longitude = pos.coords.longitude;
-        
-        console.log(latitude);
-        console.log(longitude);
-        const endpointOpenWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current&appid=${apiKey}`;
+    
+        const endpointOpenWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&lang=${lang}&units=metric`;
         getOpenWeather(endpointOpenWeather);
 
     }, 
@@ -31,7 +31,19 @@ const getOpenWeather = async (url) =>{
         const response = await fetch(url);
         if(response.ok){
             let jsonResponse = await response.json();
-            console.log(jsonResponse.timezone);
+            
+            const imgWeather = document.querySelector("#imgWeather");
+            imgWeather.innerHTML = `<img src="http://openweathermap.org/img/wn/${jsonResponse.current.weather[0].icon}@2x.png">`;
+            
+            const descriptionWeather = document.querySelector("#description-weather");
+            descriptionWeather.innerHTML = `${jsonResponse.current.weather[0].description}`;
+            
+            const currentTemperature = document.querySelector("#current-temperature");
+            currentTemperature.innerHTML = `${jsonResponse.current.temp}`;
+
+            const timeZone = document.querySelector("#timezone");
+            timeZone.innerHTML = `${jsonResponse.timezone}`;
+
         }
 
     } catch(error){
