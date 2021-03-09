66a4084c67b841a2c2318c311c19cdcc6622e6d2
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-03-09 15:50:31 +0100

    Fix insertion of correct current day

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,20 +1,14 @@
 // Handle Hours
 let currentHour = new Date().getHours();
 
-const dateBuilder = (timezone) => {
-   
-    const nowInLocalTime = Date.now()  + 1000 * (timezone / 3600);
-    const millitime = new Date(nowInLocalTime);
-
-    let day = millitime.toLocaleString("fr-FR", {weekday: "long"});
-
-    return `${day}`;
-}
-
 // Handle Days
-let currentDay = dateBuilder(Date.now());
+
+let today = new Date();
+
+let currentDay = today.toLocaleDateString("fr-FR", {weekday: "long"});
 
 const weekDays = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+
 currentDay = currentDay.charAt(0).toUpperCase() + currentDay.slice(1);
 
 let seriesOfDays = weekDays.slice(weekDays.indexOf(currentDay)).concat(weekDays.slice(0, weekDays.indexOf(currentDay)));
